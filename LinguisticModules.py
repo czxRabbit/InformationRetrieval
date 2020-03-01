5def977b7507e1cc8163a3760afc43114e89d827
CZXRabbit <1683600895@qq.com>
2020-03-01 16:34:44 +0800

    Remove all punctuation symbols and empty token

--- LinguisticModules.py
+++ LinguisticModules.py
@@ -1,14 +1,15 @@
 from nltk.stem import PorterStemmer
+import re
 
 
 def Linguistic_modules(tokens_doc_list):
     modified_tokens_doc_list = []
-    punctuation_symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '`', '~', '"', '\'',
-                           ':', ';', '|', '/', '.', ',', '?', '[', ']', '{', '}', '<', '>']
     stemmer = PorterStemmer()
     for i in range(len(tokens_doc_list)):
-        if tokens_doc_list[i][0] not in punctuation_symbols:
-            new_word = tokens_doc_list[i][0].lower()
-            new_word = stemmer.stem(new_word)
+        new_word = re.sub(r'[!@#$%^&*()\-_=+\'`~\":;|/.,?\[\]{\}<>]', "",
+                          tokens_doc_list[i][0])
+        new_word = new_word.lower()
+        new_word = stemmer.stem(new_word)
+        if new_word:
             modified_tokens_doc_list.append([new_word, tokens_doc_list[i][1]])
     return modified_tokens_doc_list
