fe2969f4a749061babbc304850d45f28f1ff1432
CZXRabbit <1683600895@qq.com>
2020-03-10 23:03:00 +0800

    Minor improvement of linguistic modules

--- LinguisticModules.py
+++ LinguisticModules.py
@@ -6,7 +6,7 @@ def Linguistic_modules(tokens_doc_list):
     modified_tokens_doc_list = []
     stemmer = PorterStemmer()
     for i in range(len(tokens_doc_list)):
-        new_word = re.sub(r'[!@#$%^&*()\-_=+\'`~\":;|/.,?\[\]{\}<>]', "",
+        new_word = re.sub(r'[!@#$%^&*()\-_=+\'`~\":;|/.,?\[\]{\}<>—]', "",
                           tokens_doc_list[i][0])
         new_word = new_word.lower()
         new_word = stemmer.stem(new_word)
